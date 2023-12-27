76f335f913a3af4958cd318ab044ebc2af382f88
Goody-23 <goodnessibikunle@gmail.com>
2023-12-26 19:14:16 -0500

    Loops

---
+++ chapter 6.py
@@ -0,0 +1,28 @@
+users = ['Goodness','OJ','Jen']
+
+data = ['Goodness','23','True']
+
+emptylist = []
+
+print("Goodness" in emptylist)
+
+print (users[0])
+print (users[-2])
+
+print(users.index('Jen'))
+print(users[0:2])
+print(users[1:])
+print(users[0:])
+print(users[-3:-1])
+
+print(len(data))
+
+users.append('Leila')
+print(users)
+
+users += ['Ben']
+print(users)
+
+users.extend(['Joseph','Jimmy'])
+print(users)
+
