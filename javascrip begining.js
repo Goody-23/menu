91a7d243f16ea9ac50c488eb52b786bbbc05f5d0
Goody-23 <goodnessibikunle@gmail.com>
2024-01-06 22:05:29 -0500

    Javascript learining

---
+++ javascrip begining.js
@@ -0,0 +1,11 @@
+var number = 5; // in-line comment
+
+/* this  is a 
+g
+g
+ghhh
+multi-line comment */
+
+number = 9;
+
+
--- loops.py
+++ loops.py
@@ -6,10 +6,13 @@ value = 1
 #     value += 1
 
 
-while value <= 10:
-    value += 1
-    if value == 5:
-        continue
-    print(value)
-else:
-    print("value is now equal to " + str(value))
+# while value <= 10:
+#     value += 1
+#     if value == 5:
+#         continue
+#     print(value)
+# else:
+#     print("value is now equal to " + str(value))
+
+
+    
