9d1ecc7b7b98a6057de2d7b36f6af38390b42e26
Goody-23 <goodnessibikunle@gmail.com>
2024-01-09 01:22:04 -0500

    Initial commit

---
+++ javascriptManny.js
@@ -0,0 +1,54 @@
+var number = 5; // in-line comment
+
+/* this  is a 
+g
+g
+ghhh
+multi-line comment */
+
+number = 9;
+
+var myName = "Goodness"
+
+// 'Var' is going to be used throught the whole program
+
+myname = 8
+
+let ourName = "freecodecamp"
+
+// Let will only be used within the scope of where you declare that
+
+
+const pi = 3.14
+
+// const is a variable that should and can never change
+
+
+var a; // (assigning variable) (end all lines in JS with semicolon)
+
+var b = 2; // We are declaring and assigning a variable
+
+console.log(a) /* This would equal null*/
+a = 7; //this was simply just assigned 
+
+b = a // assigned the content of a to b 
+
+console.log(a) /* Idk how to run right now or "roll" but when you do,
+ it'll show in the console section the number "7"*/
+
+// Initialize these 3 variables 
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1; // AWould be 5 + 1 =6
+b = b + 5;  // WOuld be 10 + 5 = 15
+c = c + " String!"; // Would be "I am a string!"
+
+
+// Case sensitivity in Variables
+// Declarations
+var StudLyCapVaR; /* This is wrong becauae the first letter should be
+lower case and the begining of another word such as "Cap or Var" in this 
+example should be Caps*/
+
